da173d58c9c1ae7347b1cc1b4306c901385c4133
Pablo Ramirez <pablocampus2023@gmail.com>
2023-05-25 21:36:33 -0500

    Mayo 25

--- js/taller2.js
+++ js/taller2.js
@@ -1,59 +1,59 @@
-////1----------------------------------------------------------//
-//let nota1 = Number(prompt('Primera nota del estudiante:'));
-//let nota2 = Number(prompt('Segunda nota del estudiante:'));
-//let nota3 = Number(prompt('Tercera nota del estudiante:'));
-//
-//function mensaje(){
-//    let promedio = (nota1 + nota2 + nota3) / 3;
-//    console.log(promedio);
-//
-//    if (promedio <= 3.9) {
-//        console.log('Estudie mijo.')
-//    }else if (promedio >3.9) {
-//        console.log('BECADO!!')
-//    }
-//}
-//mensaje();
-//
-//    
-////2----------------------------------------------------------//
-//let numero = Number(prompt('Ingrese un número entero:'));
-//if (numero%2 == 0 && numero >9) {
-//    console.log('Este número es par y mayor que 10.');
-//}else if (numero%2 == 0 && numero <10){
-//    console.log('Este número es par y menor que 10.');
-//}else if (numero%2 !== 0 && numero >9) {
-//    console.log('Este  número es impar y mayor a 10.');
-//}else {
-//    console.log('Este numero es impar y menor a 10.');
-//}
-//
-////3----------------------------------------------------------//
-//let resistencia = Number(prompt('Resistencia del circuito:'));
-//let intensidad = Number(prompt('Intensidad del circuito:'));
-//let voltaje = resistencia * intensidad;
-//console.log(`${voltaje}V`);
+//1----------------------------------------------------------//
+let nota1 = Number(prompt('Primera nota del estudiante:'));
+let nota2 = Number(prompt('Segunda nota del estudiante:'));
+let nota3 = Number(prompt('Tercera nota del estudiante:'));
 
-////4----------------------------------------------------------//
-//let edadMax = 0;
-//let nomEdadMax = '';
-//
-//for(let i = 0; i < 3; i++) {
-//    nombre = prompt('Nombre de la persona:');
-//    edad = Number(prompt('Edad de la persona:'));
-//
-//    if(edad > edadMax) {
-//        edadMax = edad;
-//        nomEdadMax = nombre;
-//    }else {
-//        edadMax = edadMax;
-//        nomEdadMax = nomEdadMax;
-//    }
-//        
-//}
-//
-//
-//console.log(`La persona de mayor edad es ${nomEdadMax} con ${edadMax} años.`);
+function mensaje(){
+    let promedio = (nota1 + nota2 + nota3) / 3;
+    console.log(promedio);
+
+    if (promedio <= 3.9) {
+        console.log('Estudie mijo.')
+    }else if (promedio >3.9) {
+        console.log('BECADO!!')
+    }
+}
+mensaje();
+
+    
+//2----------------------------------------------------------//
+let numero = Number(prompt('Ingrese un número entero:'));
+if (numero%2 == 0 && numero >9) {
+    console.log('Este número es par y mayor que 10.');
+}else if (numero%2 == 0 && numero <10){
+    console.log('Este número es par y menor que 10.');
+}else if (numero%2 !== 0 && numero >9) {
+    console.log('Este  número es impar y mayor a 10.');
+}else {
+    console.log('Este numero es impar y menor a 10.');
+}
+
+//3----------------------------------------------------------//
+let resistencia = Number(prompt('Resistencia del circuito:'));
+let intensidad = Number(prompt('Intensidad del circuito:'));
+let voltaje = resistencia * intensidad;
+console.log(`${voltaje}V`);
+
+//4----------------------------------------------------------//
+let edadMax = 0;
+let nomEdadMax = '';
+
+for(let i = 0; i < 3; i++) {
+    nombre = prompt('Nombre de la persona:');
+    edad = Number(prompt('Edad de la persona:'));
+
+    if(edad > edadMax) {
+        edadMax = edad;
+        nomEdadMax = nombre;
+    }else {
+        edadMax = edadMax;
+        nomEdadMax = nomEdadMax;
+    }
+        
+}
+
+
+console.log(`La persona de mayor edad es ${nomEdadMax} con ${edadMax} años.`);
 
 //5----------------------------------------------------------//
 let num1 = Number(prompt('Ingrese un numero:'));
